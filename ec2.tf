86257a5248c79960201b224cf9e82075cbd2da03
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-03-27 07:36:19 +0530

    terraform

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "this"{
+
+}
+resource "aws_security_group" "allow_tls"{
+name="allow_tls"
+description="Allow TLS inbound traffic and all outbound traffics"
+tags={
+Name="allow_tls"
+}
+}
