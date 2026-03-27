9ffb2c3e9b7aedb73233d749d8f01d585755f01d
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-03-27 07:55:28 +0530

    ingress and egress

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,25 @@
+# resource "aws_instance" "this"{
+
+# }
+
 resource "aws_security_group" "allow_tls"{
-name="allow_tls"
-description="Allow TLS inbound traffic and all outbound traffics"
-tags={
-Name="allow_tls"
-}
+    name="allow_tls"
+    description="Allow TLS inbound traffic and all outbound traffics"
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    tags={
+        Name="allow_tls"
+    }
 }
