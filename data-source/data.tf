4d6e35748e76e153984297561fe3da0e18a312c9
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-03-29 00:45:34 +0530

    data

--- data-source/data.tf
+++ data-source/data.tf
@@ -5,14 +5,7 @@ data "aws_ami" "joindevops"{
         name="name"
         values=["RHEL-9-DevOps-Practice"]
     }
-    filter{
-        name="root-device-type"
-        values=["ebs"]
-    }
-    filter{
-        name="virtualization-type"
-        values=["hvm"]
-    }
+    
 }
 output "ami_id"{
     value=data.aws_ami.joindevops.id
