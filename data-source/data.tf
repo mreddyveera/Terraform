4068a704294144bcba1aed723f3bf741051d8759
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-03-29 00:51:14 +0530

    root

--- data-source/data.tf
+++ data-source/data.tf
@@ -7,6 +7,14 @@ data "aws_ami" "joindevops"{
     }
     
 }
+data "aws_vpc" "default"{
+    default=true
+}
 output "ami_id"{
     value=data.aws_ami.joindevops.id
 }
+
+output "vpc_id"{
+    value=data.aws_vpc.default
+}
+
