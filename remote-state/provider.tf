3710776eeb7511ddbff7378a9ae2ce74a7b86ea0
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-03-29 18:24:38 +0530

    remote

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -10,9 +10,9 @@ terraform {
   backend "s3" {
     bucket = "manikanta-terraform-remote-state"
     key    = "expense-backend-infra"  #shouid be unique keys
-    region = "eu-north-1"
+    region = "us-east-1"
     use_lockfile=true
-    #dynamodb_table = "Manikanta-dynamodb"
+    dynamodb_table = "Manikanta-dynamodb"
   }
 }
 
