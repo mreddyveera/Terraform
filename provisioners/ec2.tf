e737b8293b3a42d77974aeca8d19a85fbe379235
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-03-29 22:08:26 +0530

    provisionetrs

---
+++ provisioners/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "Manikanta"{
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type ="t3.micro"
+    vpc_security_group_ids =[aws_security_group.Manikanta.id]
+
+    tags={
+        Name= "Hello World"
+    }
+
+    provisioner "local-exec" {
+        command="echo The server's IP address is ${aws_instance.Manikanta.private_id}"
+      
+    }
+
+}
+
+resource "aws_security_group" "Manikanta"{
+    name="Manikanta"
+    ingress{
+        from_port=22
+        to_port=22
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
